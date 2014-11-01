46dfa758bc33e2ba2ab3074023bc5cc3d0ec9b08
Alex Reichert <alex@graphscience.com>
2014-10-31 18:50:46 -0700

    Implement queue in ruby, add spec helper

--- queue/queue.rb
+++ queue/queue.rb
@@ -1,3 +1,79 @@
-class MyQueue
+# require 'pry'
 
+class MyQueue
+	attr_reader :size
+	def initialize(size)
+		@size = size
+		@data = Array.new(size)
+		@head, @tail = -1, 0
+	end
+
+	def enqueue(element)
+		if full? || element.nil?
+			nil
+		else
+			@tail -= 1
+			@data[@tail] = element
+			self
+		end
+	end
+
+	def dequeue
+		if empty?
+			nil
+		else
+			@tail += 1
+			dequeued = @data[@head]
+			@data.unshift(nil)
+			@data.pop
+			dequeued
+		end
+	end
+
+	private
+
+	def full?
+		@tail.abs == @size
+	end
+
+	def empty?
+		@head == -1 && @tail == 0
+	end
+end
+
+class MyOtherQueue
+	attr_reader :head
+	def initialize(size)
+		@size = size
+		@data = Array.new(size)
+		@head = nil
+	end
+
+	def enqueue(element)
+		raise "Queue is full" if full?
+		@head = @head.nil? ? 0 : @head + 1
+		@data[@head] = element
+		@data
+	end
+
+	def dequeue
+		raise "Queue is empty" if empty?
+		@head = @head.zero? ? nil : @head - 1
+		@data.push(nil)
+		@data.shift
+	end
+
+	def peek
+		@data
+	end
+
+	private
+
+	def empty?
+		@head.nil?
+	end
+
+	def full?
+		@head == @size
+	end
 end
---
+++ spec/queue_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+describe "Implementing a queue" do
+  it "should work" do
+  	expect(1 + 1).to eq(2)
+  end
+
+  it "should throw an error" do
+  	expect(1 + 1).to eq(3)
+  end
+end
