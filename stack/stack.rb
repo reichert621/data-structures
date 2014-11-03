58e9cb3357a7383691c7a69d3e7de632165d7837
Alex Reichert <alex@graphscience.com>
2014-11-03 15:24:18 -0800

    Implement basic stack in ruby

--- stack/stack.rb
+++ stack/stack.rb
@@ -1,3 +1,37 @@
 class MyStack
+  attr_reader :data, :size
+  def initialize(size)
+    @size = size
+    @data = Array.new(@size)
+    @top = -1
+  end
 
+  def push(element)
+    raise "Stack is full" if full?
+    @top += 1
+    @data[@top] = element
+    self
+  end
+
+  def pop
+    raise "Stack is empty" if empty?
+    popped = @data[@top]
+    @data[@top] = nil
+    @top -= 1
+    popped
+  end
+
+  def peek
+    @data[@top]
+  end
+
+  private
+
+  def empty?
+    @top == -1
+  end
+
+  def full?
+    @top == (@size - 1)
+  end
 end
